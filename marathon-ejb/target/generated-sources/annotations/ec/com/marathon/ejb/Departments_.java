b15e2c9194fe0b7e83806f41cd4008c15ff80b4c
Esteban Ortiz <eortiz@mmconsultingtax.com>
2023-07-20 16:18:34 -0500

    Primer commit :)

---
+++ marathon-ejb/target/generated-sources/annotations/ec/com/marathon/ejb/Departments_.java
@@ -0,0 +1,28 @@
+package ec.com.marathon.ejb;
+
+import ec.com.marathon.ejb.Departments;
+import ec.com.marathon.ejb.DepartmentsEmployees;
+import java.util.Date;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.ListAttribute;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2023-07-20T00:40:23")
+@StaticMetamodel(Departments.class)
+public class Departments_ { 
+
+    public static volatile SingularAttribute<Departments, Date> createdDate;
+    public static volatile ListAttribute<Departments, DepartmentsEmployees> departmentsEmployeesList;
+    public static volatile SingularAttribute<Departments, String> createdBy;
+    public static volatile SingularAttribute<Departments, String> phone;
+    public static volatile ListAttribute<Departments, Departments> departmentsList;
+    public static volatile SingularAttribute<Departments, Departments> idEnterprise;
+    public static volatile SingularAttribute<Departments, Date> modifiedDate;
+    public static volatile SingularAttribute<Departments, String> name;
+    public static volatile SingularAttribute<Departments, String> description;
+    public static volatile SingularAttribute<Departments, String> modifiedBy;
+    public static volatile SingularAttribute<Departments, Integer> id;
+    public static volatile SingularAttribute<Departments, Boolean> status;
+
+}
---
+++ marathon-ejb/target/generated-sources/annotations/ec/com/marathon/ejb/Enterprises_.java
@@ -0,0 +1,22 @@
+package ec.com.marathon.ejb;
+
+import java.util.Date;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2023-07-20T00:40:23")
+@StaticMetamodel(Enterprises.class)
+public class Enterprises_ { 
+
+    public static volatile SingularAttribute<Enterprises, Date> createdDate;
+    public static volatile SingularAttribute<Enterprises, String> address;
+    public static volatile SingularAttribute<Enterprises, String> createdBy;
+    public static volatile SingularAttribute<Enterprises, String> phone;
+    public static volatile SingularAttribute<Enterprises, Date> modifiedDate;
+    public static volatile SingularAttribute<Enterprises, String> name;
+    public static volatile SingularAttribute<Enterprises, String> modifiedBy;
+    public static volatile SingularAttribute<Enterprises, Integer> id;
+    public static volatile SingularAttribute<Enterprises, Boolean> status;
+
+}
